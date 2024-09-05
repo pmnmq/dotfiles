c5c51e8f5e5100b426b8269fece9c3829ff3a032
chun <pujichun@outlook.com>
2024-09-05 14:41:44 +0800

    fix: fix cargo env

---
+++ fish/conf.d/rustup.fish
@@ -0,0 +1,1 @@
+source "/Users/pujic/.local/share/cargo/env.fish"
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,3 +1,5 @@
+OS="$(uname)"
+
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_DATA_HOME=$HOME/.local/share
@@ -9,6 +11,9 @@ export PATH=$XDG_BIN_HOME:$PATH
 # Rust
 export RUSTUP_HOME=$XDG_DATA_HOME/rustup
 export CARGO_HOME=$XDG_DATA_HOME/cargo
+if test -d $CARGO_HOME; then
+	export PATH=$CARGO_HOME:$PATH
+fi
 
 # Go
 if test -d /usr/local/bin; then
@@ -22,12 +27,12 @@ export GOPROXY="https://goproxy.cn"
 export GOMODCACHE=$XDG_CACHE_HOME/go/mod
 
 # npm && pnpm
-export NPM_CONFIG_REGISTRY=https://registry.example.com
+export NPM_CONFIG_REGISTRY=https://registry.npmmirror.com
 export NPM_CONFIG_PREFIX=$XDG_DATA_HOME/npm
 export NPM_CONFIG_CACHE=$XDG_CACHE_HOME/npm
 
 if test -d $NPM_CONFIG_PREFIX; then
-	export PATH=$NPM_CONFIG_PREFIX/npm/bin:$PATH
+	export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
 fi
 
 export PNPM_HOME=$XDG_DATA_HOME/pnpm
@@ -35,8 +40,8 @@ if test -d $PNPM_HOME; then
 	export PATH=$PNPM_HOME/bin:$PATH
 fi
 
-# ranger
-export RANGER_LOAD_DEFAULT_RC=FALSE
+export YARN_CACHE_FOLDER=$XDG_CACHE_HOME/yarn
+export YARN_REGISTRY=$NPM_CONFIG_REGISTRY
 
 # dotnet
 export PATH=$PATH:/usr/local/dotnet
@@ -51,3 +56,20 @@ export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottle
 export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
 export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 export HOMEBREW_NO_AUTO_UPDATE=true
+
+# jdk
+if test -d "/usr/local/jdk17"; then
+	jdk_path="/usr/local/jdk17/Contents/Home"
+	if test $OS = "Linux"; then
+		jdk_path="/usr/local/jdk17"
+	fi
+	export JAVA_HOME=$jdk_path
+	export PATH=$JAVA_HOME/bin:$PATH
+fi
+
+# gradle
+if test -d "/usr/local/gradle"; then
+	export GRADLE_HOME="/usr/local/gradle"
+	export PATH=$GRADLE_HOME/bin:$PATH
+fi
+export GRADLE_USER_HOME=$XDG_CACHE_HOME/gradle
