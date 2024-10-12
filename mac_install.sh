8f2d950d31b5add4a5eb78ec3ab3e2f2e927cef6
chun <pujichun@outlook.com>
2024-10-12 20:11:24 +0800

    refactor: 安装脚本只在mac下使用

--- mac_install.sh
+++ mac_install.sh
@@ -2,8 +2,6 @@
 
 OS="$(uname)"
 
-packages=("git" "zip" "unzip" "wget")
-
 clone_config() {
 	rm -rf $HOME/.config
 	git clone -depth=1 https://github.com/pmnmq/.config $HOME/.config
@@ -41,31 +39,15 @@ install_homebrew() {
 	export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 	export HOMEBREW_INSTALL_FROM_API=1
 	/bin/bash -c "$(curl -fsSL https://gitee.com/pujichun/install-script/raw/master/homebrew-install.sh)"
+	if test -x /opt/homebrew/bin/brew; then
+		eval "$(/opt/homebrew/bin/brew shellenv)"
+	fi
 	brew tap --custom-remote --force-auto-update homebrew/cask-fonts https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-fonts.git
 	brew tap --custom-remote --force-auto-update homebrew/cask-versions https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-versions.git
 	brew tap --custom-remote --force-auto-update homebrew/command-not-found https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-command-not-found.git
 	brew tap --custom-remote --force-auto-update homebrew/services https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-services.git
 	brew update
 	brew install brew-cask
-
-}
-
-install_linux_base_soft() {
-	if [-f "/etc/arch-release" ]; then
-		sudo pacman -S --needed base-devel fd zsh
-		sudo pacman -S "$packages[@]" python3
-		git clone https://aur.archlinux.org/paru.git
-		cd paru
-		makepkg -si
-		cd
-	fi
-
-	if [ -f "/etc/debian_version" ]; then
-		sudo apt install -y "$packages[@]" python3 python3-dev zsh
-
-		# install fd
-		sudo apt install fd-find
-	fi
 }
 
 config_zsh() {
@@ -84,31 +66,31 @@ config_zsh() {
 }
 
 install_mac_base_soft() {
-	if [[ "${OS}" == "Darwin" ]]; then
-		brew install "$packages[@]"
-		brew install --cask iina
-		brew install --cask app-cleaner
-		brew install --case devtoys
-		brew install --case permute
-		brew install --cask transmit
-		brew install --cask maccy
-		brew install --cask itsycal
-		brew install --cask iterm2
-		brew install 1password-cli
-		brew install mas
-		# 腾讯会议
-		mas install 1484048379
-		# 1Password for Safari
-		mas install 1569813296
-		mas install 985367838
-
-	fi
+	brew install --cask iina
+	brew install --cask appcleaner
+	brew install --case devtoys
+	brew install --case permute
+	brwe install --case downie
+	brew install --cask transmit
+	brew install --cask maccy
+	brew install --cask itsycal
+	brew install --cask iterm2
+	brew install --case netnewswire
+	brew install 1password-cli
+	brew install mas
+	# 腾讯会议
+	mas install 1484048379
+	# 1Password for Safari
+	mas install 1569813296
+	mas install 985367838
+	# localsend
+	mas install 1661733229
 }
 
 brew_install_common_soft() {
 	brew install yazi ffmpegthumbnailer sevenzip jq poppler ripgrep zoxide imagemagick font-symbols-only-nerd-font
-	brew install --cask telegram
 	brew install --cask kitty
+	brew install --cask warp
 	brew install --cask picgo
 	brew install --cask google-chrome
 	brew install --cask visual-studio-code
@@ -117,9 +99,10 @@ brew_install_common_soft() {
 	brew install --cask jetbrains-toolbox
 	brew install --cask hoppscotch
 	brew install neovim neofetch ruff lazygit glow hugo graphviz
-	brew tap localsend/localsend
-	brew install localsend
-	brew install mise
+	if test "$OS" != "Darwin"; then
+		brew tap localsend/localsend
+		brew install localsend
+	fi
 	brew install protobuf
 	brew install protoc-gen-go
 	brew install protoc-gen-go-grpc
@@ -127,41 +110,18 @@ brew_install_common_soft() {
 	brew install fzf
 }
 
-set_proxy() {
-	read -p "Enter the HTTP proxy (e.g., http://user:password@proxy-server:port): " http_proxy
-	export http_proxy=$http_proxy
-	export https_proxy=$http_proxy
-	echo "HTTP proxy set to $http_proxy"
-}
-
 start_install() {
-	read -p "Do you want set an HTTP/HTTPS proxy? (y/n): " use_proxy
-	use_proxy=${use_proxy,,}
-	if [[ "$use_proxy" == "y" ]]; then
-		set_proxy
-	elif [[ "$use_proxy" == "n" ]]; then
-		unset http_proxy
-		unset https_proxy
-	else
-		echo "Invalid input. Please enter 'y' for yes or 'n' for no."
-	fi
-
-	if [[ "${OS}" == "Darwin" ]]; then
-		echo "MacOS exec xcode-select --install"
-		sudo xcode-select --install
-	fi
-
 	clone_config
 
 	install_rust_toolchain
 
-	install_linux_base_soft
+	install_homebrew
 
 	config_zsh
-
-	install_homebrew
 
 	install_mac_base_soft
 
 	brew_install_common_soft
 }
+
+start_install
