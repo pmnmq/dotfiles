225bf934594d7d076950648630839aaa24f8c956
pmnmq <pujichun@outlook.com>
2025-08-31 15:03:45 +0800

    feat: 调整光标

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,8 @@ config = {
 	initial_rows = 30,
 	font = wezterm.font("FiraCode Nerd Font"),
 	enable_tab_bar = false,
-	font_size = 12,
+	font_size = 17,
+	cursor_blink_rate = 0,
 	color_scheme = "Dracula (Official)",
 	window_close_confirmation = "NeverPrompt",
 	window_padding = {
@@ -17,13 +18,13 @@ config = {
 		bottom = 30,
 	},
 }
-local target = wezterm.target_triple
-
--- macOS 上显示 window_decorations，Linux 隐藏
-if target:find("apple") then
-	config.window_decorations = "TITLE"
-else
-	config.window_decorations = "NONE"
-end
+-- local target = wezterm.target_triple
+--
+-- -- macOS 上显示 window_decorations，Linux 隐藏
+-- if target:find("apple") then
+-- 	config.window_decorations = "TITLE"
+-- else
+-- 	config.window_decorations = "NONE"
+-- end
 
 return config
