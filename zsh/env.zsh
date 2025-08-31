b3d7ec8cbf421da7cab63bbd2aad0bc6c258abc9
pmnmq <pujichun@outlook.com>
2025-08-31 15:07:14 +0800

    chore: format

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -12,13 +12,13 @@ export PATH=$XDG_BIN_HOME:$PATH
 export RUSTUP_HOME=$XDG_DATA_HOME/rustup
 export CARGO_HOME=$XDG_DATA_HOME/cargo
 if test -d $CARGO_HOME; then
-	export PATH=$CARGO_HOME/bin:$PATH
+    export PATH=$CARGO_HOME/bin:$PATH
 fi
 
 # Go
 if test -d /usr/local/go/bin; then
-	export GOROOT=/usr/local/go
-	export PATH=$GOROOT/bin:$PATH
+    export GOROOT=/usr/local/go
+    export PATH=$GOROOT/bin:$PATH
 fi
 export GOPATH=$HOME/.local/share/go
 export GOBIN=$GOPATH/bin
@@ -32,13 +32,13 @@ export NPM_CONFIG_PREFIX=$XDG_DATA_HOME/npm
 export NPM_CONFIG_CACHE=$XDG_CACHE_HOME/npm
 
 if test -d $NPM_CONFIG_PREFIX; then
-	export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
+    export PATH=$NPM_CONFIG_PREFIX/bin:$PATH
 fi
 
 export PNPM_HOME=$XDG_DATA_HOME/pnpm
 if test -d $PNPM_HOME; then
-	export PATH=$PNPM_HOME/bin:$PATH
-	export PATH=$PNPM_HOME:$PATH
+    export PATH=$PNPM_HOME/bin:$PATH
+    export PATH=$PNPM_HOME:$PATH
 fi
 
 export YARN_CACHE_FOLDER=$XDG_CACHE_HOME/yarn
@@ -60,18 +60,18 @@ export HOMEBREW_NO_AUTO_UPDATE=true
 
 # jdk
 if test -d "/usr/local/jdk17"; then
-	jdk_path="/usr/local/jdk17/Contents/Home"
-	if test $ZSHRC_FLAG_OS = "Linux"; then
-		jdk_path="/usr/local/jdk17"
-	fi
-	export JAVA_HOME=$jdk_path
-	export PATH=$JAVA_HOME/bin:$PATH
+    jdk_path="/usr/local/jdk17/Contents/Home"
+    if test $ZSHRC_FLAG_OS = "Linux"; then
+        jdk_path="/usr/local/jdk17"
+    fi
+    export JAVA_HOME=$jdk_path
+    export PATH=$JAVA_HOME/bin:$PATH
 fi
 
 # gradle
 if test -d "/usr/local/gradle"; then
-	export GRADLE_HOME="/usr/local/gradle"
-	export PATH=$GRADLE_HOME/bin:$PATH
+    export GRADLE_HOME="/usr/local/gradle"
+    export PATH=$GRADLE_HOME/bin:$PATH
 fi
 export GRADLE_USER_HOME=$XDG_CACHE_HOME/gradle
 
